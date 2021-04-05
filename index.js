0515862fc963e3e06bd855ea47422f3cd5a8a392
Felipe Nascimento <flpnascto@gmail.com>
2021-04-05 18:56:58 -0300

    Requisito 03

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const rescue = require('express-rescue');
 const fs = require('fs').promises;
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,6 +19,33 @@ const apiData = async () => {
 
   return readData;
 };
+
+function notNullOrEmpty(value) {
+  if (!value || value === '') return true;
+  return false;
+}
+
+function validateEmail(email) {
+  const mailFormat = /\b[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\b/g;
+
+  if (notNullOrEmpty(email)) {
+    throw new Error('O campo "email" é obrigatório');
+  }
+
+  if (!email.match(mailFormat)) {
+    throw new Error('O "email" deve ter o formato "email@email.com"');
+  }
+}
+
+function validadePassword(password) {
+  if (notNullOrEmpty(password)) {
+    throw new Error('O campo "password" é obrigatório');
+  }
+
+  if (password.toString().length < 6) {
+    throw new Error('A "senha" deve ter pelo menos 6 caracteres');
+  }
+}
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -39,6 +67,21 @@ app.get('/crush/:id', rescue(async (req, res) => {
   }
   res.json(artist);
 }));
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    validateEmail(email);
+    validadePassword(password);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+
+  const token = crypto.randomBytes(8).toString('hex');
+
+  res.json({ token });
+});
 
 app.use((err, req, res) => {
   res.status(err.code).send(err.error);
